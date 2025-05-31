d439b2202786873cc55211a4a983df265726f306
shafeeq777444 <kp.shafeeqmohamed@gmail.com>
2025-05-31 07:41:15 +0530

    json-server hosted

--- src/service/axiosInstance.js
+++ src/service/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const axiosInstance=axios.create({
-    baseURL:"http://localhost:5001"
+    baseURL:"bcyclejson-production.up.railway.app"
 })
 export default axiosInstance
